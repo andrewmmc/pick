887e436f6b00a209a6798dd602ce7f44d73c8de8
Andrew Mok <hello@andrewmmc.com>
2019-02-15 22:56:20 +0800

    ADD: Event app_mention

--- function.go
+++ function.go
@@ -4,21 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
+	"log"
 	"net/http"
 )
 
-type body struct {
+type Body struct {
 	Challenge string `json:"challenge"`
+	Event     Event
+}
+
+type Event struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
 }
 
 // respond with the challenge parameter value
 func ReceiveEvent(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	var d body
-	err := decoder.Decode(&d)
+	var payload Body
+	err := decoder.Decode(&payload)
 	if err != nil {
 		fmt.Fprint(w, "Error!")
 		return
 	}
-	fmt.Fprint(w, html.EscapeString(d.Challenge))
+
+	if payload.Challenge != "" {
+		fmt.Fprint(w, html.EscapeString(payload.Challenge))
+		return
+	}
+
+	if payload.Event.Type == "app_mention" {
+		log.Println(payload.Event.Text)
+		return
+	}
 }
