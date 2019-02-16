1a9f1a3b299f5af083bd99c33a0af16972535ab6
Andrew Mok <hello@andrewmmc.com>
2019-02-16 12:35:10 +0800

    WIP

--- function.go
+++ function.go
@@ -1,6 +1,7 @@
 package function
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html"
@@ -10,15 +11,24 @@ import (
 
 type Body struct {
 	Challenge string `json:"challenge"`
+	Token     string `json:"token"`
+	TeamID    string `json:"team_id"`
+	APIAppID  string `json:"api_app_id"`
 	Event     Event
 }
 
 type Event struct {
-	Type string `json:"type"`
-	Text string `json:"text"`
+	Channel string `json:"channel"`
+	Type    string `json:"type"`
+	Text    string `json:"text"`
+	User    string `json:"user"`
 }
 
-// respond with the challenge parameter value
+type Message struct {
+	Text    string `json:"text"`
+	Channel string `json:"channel"`
+}
+
 func ReceiveEvent(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	var payload Body
@@ -29,12 +39,35 @@ func ReceiveEvent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if payload.Challenge != "" {
+		// respond with the challenge parameter value
 		fmt.Fprint(w, html.EscapeString(payload.Challenge))
 		return
 	}
 
 	if payload.Event.Type == "app_mention" {
+		log.Println(payload.Token)
 		log.Println(payload.Event.Text)
+		log.Println(payload.Event.User)
+		log.Println(payload.Event.Channel)
+		SendMessage()
+		return
+	}
+}
+
+func SendMessage() {
+	client := &http.Client{}
+	m := Message{"testing", ""}
+	b, _ := json.Marshal(m)
+	req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", bytes.NewBuffer(b))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer ")
+	if err != nil {
+		log.Println("Error")
+		return
+	}
+	_, err = client.Do(req)
+	if err != nil {
+		log.Println("Error")
 		return
 	}
 }
